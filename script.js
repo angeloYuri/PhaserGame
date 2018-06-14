6f7dcd2afc0173c095fa8894ab8838af85fdf802
fudangs <adilson.dti@feb.br>
2018-06-14 14:46:36 -0300

    Parrots descendo e limites nos piercing

--- script.js
+++ script.js
@@ -18,10 +18,12 @@ var cursors;
 var qntAlvos = 20;
 var contador = 0;
 var bool = true;
-
+var contadorT = 0;
+var qntPiercing = 3;
 var bulletTime = 0;
 var bullet;
-
+var acertos
+var tiros;
 function create() {
 
     game.stage.backgroundColor = '#2d2d2d';
@@ -53,18 +55,23 @@ function create() {
 }
 
 function criarJogo(){
-  skill = game.add.group();
-  skill.enableBody = true;
-  skill.physicsBodyType = Phaser.Physics.ARCADE;
+    contadorT = 0;
+    acertos = 0;
+    tiros = 0;
+    pct = 0;
+    attPlacar();
+    skill = game.add.group();
+    skill.enableBody = true;
+    skill.physicsBodyType = Phaser.Physics.ARCADE;
 
   veggies = game.add.group();
   veggies.enableBody = true;
   veggies.physicsBodyType = Phaser.Physics.ARCADE;
-
-  var d = skill.create(game.world.randomX, Math.random() * 500, 'skill', game.rnd.integerInRange(0, 36));
-  d.name = 'skillPiercing';
-  d.body.imovable = true;
-
+  for(var i = 0; i < qntPiercing; i++){
+    var d = skill.create(game.world.randomX, Math.random() * 500, 'skill', game.rnd.integerInRange(0, 36));
+    d.name = 'skillPiercing' + i;
+    d.body.imovable = true;
+  }
   for (var i = 0; i < qntAlvos; i++)
   {
       var c = veggies.create(game.world.randomX, Math.random() * 500, 'veggies', game.rnd.integerInRange(0, 36));
@@ -78,7 +85,8 @@ function update() {
     //  As we don't need to exchange any velocities or motion we can the 'overlap' check instead of 'collide'
     game.physics.arcade.overlap(bullets, veggies, collisionHandler, null, this);
     game.physics.arcade.overlap(bullets, skill, collisionHandler2, null, this);
-
+    skill.setAll('y', 1, true, true, 1);
+    veggies.setAll('y', 1, true, true, 1);
     sprite.body.velocity.x = 0;
     sprite.body.velocity.y = 0;
 
@@ -100,6 +108,8 @@ function update() {
 
 function fireBullet () {
 
+
+
     if (game.time.now > bulletTime)
     {
         bullet = bullets.getFirstExists(false);
@@ -109,6 +119,8 @@ function fireBullet () {
             bullet.reset(sprite.x + 35, sprite.y - 8);
             bullet.body.velocity.y = -300;
             bulletTime = game.time.now + 150;
+            tiros++;
+            attPlacar();
         }
     }
 
@@ -123,10 +135,14 @@ function resetBullet (bullet) {
 
 //  Called if the bullet hits one of the veg sprites
 function collisionHandler (bullet, veg) {
-
-    if(bool){
+    acertos++;
+    attPlacar();
+    if(contadorT>0){
+      contadorT--;
+    }else{
       bullet.kill();
     }
+
     veg.kill();
     contador++;
     if(contador == qntAlvos){
@@ -139,9 +155,22 @@ function collisionHandler (bullet, veg) {
 }
 
 function collisionHandler2 (bullet, skill) {
-
+    contadorT = 6;
     bullet.kill();
     skill.kill();
     bool = false;
+    acertos++;
+    attPlacar();
+}
 
+function attPlacar(){
+  if(acertos == 0 && tiros == 0){
+  pct = 0;
+}else{
+  pct = acertos/tiros;
+  pct *= 100;
+}
+  $("#tiros").html(tiros);
+  $("#acertos").html(acertos);
+  $("#pct").html(pct.toFixed(0) + "%");
 }
